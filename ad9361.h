acf246c3b0e3835de0b1c43eb8207417c83d3764
Artem Anohda <anohda.a@ajax.systems>
2024-05-16 15:01:26 +0300

    upd

--- ad9361.h
+++ ad9361.h
@@ -40,11 +40,12 @@ extern "C" {
 /** @} */
 
 #ifdef _WIN32
-#   ifdef LIBAD9361_EXPORTS
-#	define __api __declspec(dllexport)
-#   else
-#	define __api __declspec(dllimport)
-#   endif
+// #   ifdef LIBAD9361_EXPORTS
+// #	define __api __declspec(dllexport)
+// #   else
+// #	define __api __declspec(dllimport)
+// #   endif
+#define __api
 #elif __GNUC__ >= 4 && !defined(MATLAB_MEX_FILE) && !defined(MATLAB_LOADLIBRARY)
 #   define __api __attribute__((visibility ("default")))
 #else
